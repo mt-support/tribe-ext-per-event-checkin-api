c6f910dbc58267fd4f5f747d76c424a6da7212da
rafsuntaskin <rafsun.aiub@gmail.com>
2021-05-03 14:58:25 +0600

    use text domain and minor adjustments

--- src/Tribe/Api_Handler.php
+++ src/Tribe/Api_Handler.php
@@ -19,15 +19,18 @@ namespace Tribe\Extensions\Per_Event_Checkin;
 class Api_Handler {
 
 	/**
+     * Meta key name for storing Event API key.
+	 */
+    public $api_meta_key = '_et_event_api_key';
+
+	/**
      * Generate API code for Event with Tickets/RSVP.
      *
 	 * @param int $post_id Event or Post ID.
 	 */
 	public function generate_api_code_for_event( $post_id ) {
 
-		$api_meta_key = '_et_event_api_key';
-
-		$api_key = get_post_meta( $post_id, $api_meta_key, true );
+		$api_key = get_post_meta( $post_id, $this->api_meta_key, true );
 
 		// If API key is already generated, return.
 		if ( ! empty( $api_key ) ) {
@@ -38,7 +41,7 @@ class Api_Handler {
 
 		$api_key = $qr_settings->generate_new_api();
 
-		update_post_meta( $post_id, $api_meta_key, $api_key );
+		update_post_meta( $post_id, $this->api_meta_key, $api_key );
 	}
 
 	/**
@@ -50,6 +53,11 @@ class Api_Handler {
 	 * @return bool
 	 */
 	function alter_default_api_with_event_api( $is_valid, $qr_data ) {
+
+	    // If already valid, bail out.
+	    if ( $is_valid ) {
+	        return $is_valid;
+        }
 
 		if ( ! isset( $qr_data['api_key'] ) ) {
 			return $is_valid;
@@ -93,9 +101,15 @@ class Api_Handler {
 			return;
 		}
 
+		$api_key = get_post_meta( $post->ID, $this->api_meta_key, true );
+
+		if ( empty( $api_key ) ) {
+			return;
+		}
+
 		add_meta_box(
 			'event_tickets_event_api',
-			'Event Check-in API',
+			__( 'Event Check-in API', 'et-per-event-checkin' ),
 			[ $this, 'render_event_tickets_api_meta_box' ],
 			null,
 			'side'
@@ -109,16 +123,12 @@ class Api_Handler {
 	 */
 	function render_event_tickets_api_meta_box( $post ) {
 
-	    $api_meta_key = '_et_event_api_key';
-		$api_key      = get_post_meta( $post->ID, $api_meta_key, true );
+		$api_key = get_post_meta( $post->ID, $this->api_meta_key, true );
 
-		if ( empty( $api_key ) ) {
-		    return;
-        }
-
+		$kb_url = 'https://theeventscalendar.com/knowledgebase/k/using-qr-codes-with-event-tickets-plus/';
 		?>
 		<input type="text" disabled value="<?php esc_attr_e( $api_key ); ?>">
-		<p class="help-block">Copy this API into the QR Code app to allow checkin for this Event Only.</p>
+		<p><?php printf( wp_kses( 'Copy this API into the <a href="%s" target="_blank" rel="noopener noreferrer">QR Code app</a> to allow checkin for this Event Only.', 'et-per-event-checkin' ), esc_attr( $kb_url ) ); ?></p>
 		<?php
 	}
 }
